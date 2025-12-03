9767fa855de65e0e785e1ef90d715547486e1fe4
Omar EL FILALI <omarelfiie2007@gmail.com>
2025-12-03 16:53:41 +0100

    added the add_icon.png

--- index.php
+++ index.php
@@ -7,44 +7,25 @@ include("database.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Document</title>
+    <script src="script.js" defer></script>
+    <title>Manager</title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body class="flex justify-center">
-    <form action="index.php" method="post" class="flex flex-col w-[40%] p-10">
-        <h2 class="text-4xl">Welcome</h2>
-        <br>
-        <label for="username">username:<br></label>
-        <input class="border p-1 rounded-[5px]" placeholder="username" type="text" name="username" id="username">
-        <label for="pass">password:<br></label>
-        <input class="border p-1 rounded-[5px]" placeholder="password" type="password" name="pass" id="pass">
-        <button class="cursor-pointer" type="submit">submit</button>
-    </form>
+    <nav>
+        <a class="absolute top-3 border" href="manager.php" id="add"></a>
+    </nav>
+
+    <main>
+        <section id="balance">
+            
+        </section>
+    </main>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = $_POST["pass"];
 
-        if (empty($username)) {
-            echo "plase enter a username";
-        } elseif (empty($password)) {
-            echo "Please enter a password";
-        } else {
-            $check_sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
-            $result = mysqli_query($conn, $check_sql);
-
-            if (mysqli_num_rows($result) > 0) {
-                echo "This username is already taken.";
-            }else{
-                $hash = password_hash($password, PASSWORD_DEFAULT);
-                $sql = "INSERT INTO users (username, password)
-                            VALUES ('$username', '$hash')";
-                mysqli_query($conn, $sql);
-                echo "You are now Registered!";
-            }
-        }
-    }
-    mysqli_close($conn);
+        mysqli_close($conn);
     ?>
 </body>
 
